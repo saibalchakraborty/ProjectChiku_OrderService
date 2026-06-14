563017931d6b5846f8d77020bb1e5e5029badbcb
Saibal Chakraborty <saibal183@gmail.com>
2026-06-14 21:02:56 +0530

    feature: add recorder jar

--- src/main/java/com/chiku/order/service/OrderServiceApplication.java
+++ src/main/java/com/chiku/order/service/OrderServiceApplication.java
@@ -3,7 +3,10 @@ package com.chiku.order.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.chiku.order.service",
+		"com.chiku.recorder"
+})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
